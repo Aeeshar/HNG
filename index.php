7268d69a7d986589ddd707b9bfff893022927da8
Aisha Amoka <aishaamoka@gmail.com>
2018-04-17 13:50:22 +0100

    WomeninSTEMHNG4.0 Task

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+
+<link rel="stylesheet" href="style.css">
+<!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
+<script>var __adobewebfontsappname__="dreamweaver"</script>
+<script src="http://use.edgefonts.net/rouge-script:n4:default;satisfy:n4:default.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+<div>
+<div class="winstem">
+<h1>Women in STEM</h1></div>
+<div class="hng">
+  <p>...HNA 4.0</p>
+</div>
+   <div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+  <div class="date"><font color="#140E46"><?php
+  date_default_timezone_set("Africa/Lagos");
+echo "The time is " . date("h:i:sa");
+?></font></div>
+</div>
+ </body>
+</html>
